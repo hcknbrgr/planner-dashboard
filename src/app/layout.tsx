ba7ff5805ac8eb45d67e81efe3959468dea69f29
hcknbrgr <hcknbrgr@gmail.com>
2024-12-26 22:45:04 -0500

    remove unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,6 @@ import "./style.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "LLM Todo List",
   description: "A simple UI to assist with todo lists",
